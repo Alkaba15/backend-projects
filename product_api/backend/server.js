76e0ccd8c7aa0cac6606595329209ff769b9e536
Alseny Kaba <alsenykaba98@gmail.com>
2025-10-07 08:49:57 -0300

    feat: add a product to database via post request

--- product_api/backend/server.js
+++ product_api/backend/server.js
@@ -1,14 +1,34 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import {connectDb} from './config/db.js'
+import { Product } from './models/Product.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = 3000
-
-app.get('/', (req, res)=>{
-      res.send('Hello Express')
+app.use(express.json())
+app.post('/api/products', async(req, res)=>{
+    const product = req.body
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({
+            succes : false, 
+            message:"Please fill all the fields"})
+    }
+    const newProduct = new Product(product)
+    try {
+        await newProduct.save()
+        res.status(201).json({
+            success: true,
+            message : 'Product successfuly created',
+            product : newProduct
+        })
+    }catch(error) {
+        res.status(500).json({
+            success : false,
+            message : 'internal server error'
+        })
+    }
 })
 
 app.listen(PORT, ()=>{
